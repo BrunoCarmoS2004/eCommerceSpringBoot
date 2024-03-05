a7a7500513e9034623dcf6272a7d4377b2d7a08c
Bruno do Carmo Souza <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-03-05 19:38:34 -0300

    Inicio do projeto

---
+++ eCormmerce/src/main/java/com/br/eCormmerce/ECormmerceApplication.java
@@ -0,0 +1,13 @@
+package com.br.eCormmerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECormmerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECormmerceApplication.class, args);
+	}
+
+}
