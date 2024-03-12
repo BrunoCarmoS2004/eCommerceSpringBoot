4802cc4b45c55f1cf037a1ede6effc9bcc5677ff
Bruno do Carmo Souza <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-03-11 21:26:54 -0300

    vinculando banco de dados h2

--- eCormmerce/src/main/java/com/br/eCormmerce/controllers/ProdutosController.java
+++ eCormmerce/src/main/java/com/br/eCormmerce/controllers/ProdutosController.java
@@ -2,8 +2,7 @@ package com.br.eCormmerce.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
