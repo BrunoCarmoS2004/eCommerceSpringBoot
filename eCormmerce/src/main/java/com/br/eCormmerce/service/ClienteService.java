ff1bb5aec8b64a7aba26a670b9d51b2f87856993
ribeirogabrieldev <128650497+ribeirogabrieldev@users.noreply.github.com>
2024-03-19 19:44:07 -0300

    commit

---
+++ eCormmerce/src/main/java/com/br/eCormmerce/service/ClienteService.java
@@ -0,0 +1,14 @@
+import com.example.demo.model.Funcionario;
+import com.example.demo.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClinteService {
+
+    @Autowired
+    ClienteService clienteService
+
+    public 
+
+}
