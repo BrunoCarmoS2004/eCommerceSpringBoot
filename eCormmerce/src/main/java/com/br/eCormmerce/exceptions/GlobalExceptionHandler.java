1d80e9b1dd3ecdf9c07c85f09a0f07b0f163cc86
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-03-24 21:50:10 -0300

    Create GlobalExceptionHandler.java

---
+++ eCormmerce/src/main/java/com/br/eCormmerce/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.br.eCormmerce.exceptions;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(
+            MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+}
