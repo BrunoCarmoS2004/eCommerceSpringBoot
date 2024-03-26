fa0758077e257c77ed5efa170c4b2436d90314c3
Bruno do Carmo Souza <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-03-26 19:05:51 -0300

    Update Vendas.java

--- eCormmerce/src/main/java/com/br/eCormmerce/models/Vendas.java
+++ eCormmerce/src/main/java/com/br/eCormmerce/models/Vendas.java
@@ -4,8 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+
 
 @Entity
 public class Vendas {
