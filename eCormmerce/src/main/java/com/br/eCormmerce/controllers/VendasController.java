27a6c8a2c8d1b9a139ce41d7947b71898d8fe279
Bruno do Carmo Dev <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-03-30 15:58:38 -0300

    Update VendasController.java

--- eCormmerce/src/main/java/com/br/eCormmerce/controllers/VendasController.java
+++ eCormmerce/src/main/java/com/br/eCormmerce/controllers/VendasController.java
@@ -2,22 +2,13 @@ package com.br.eCormmerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.eCormmerce.models.Cliente;
-import com.br.eCormmerce.models.Produtos;
 import com.br.eCormmerce.models.Vendas;
-import com.br.eCormmerce.models.Vendedor;
-import com.br.eCormmerce.repositorys.ClienteRepository;
-import com.br.eCormmerce.repositorys.ProdutosRepository;
-import com.br.eCormmerce.repositorys.VendedorRepository;
 import com.br.eCormmerce.service.VendasServices;
 
 import jakarta.validation.Valid;
@@ -27,9 +18,6 @@ import jakarta.validation.Valid;
 public class VendasController {
   @Autowired
   private VendasServices vendasServices;
-  private ClienteRepository clienteRepository;
-  private VendedorRepository vendedorRepository;
-  private ProdutosRepository produtosRepository;
 
   @PostMapping("/comprar")
   public ResponseEntity<Object>criarVenda(@Valid @RequestBody Vendas venda){
