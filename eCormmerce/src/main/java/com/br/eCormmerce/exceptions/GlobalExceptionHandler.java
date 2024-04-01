ee985d168606ba060d5263ef3131f60ea49edf10
Bruno do Carmo Souza <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-04-01 14:20:27 -0300

    Update GlobalExceptionHandler.java

--- eCormmerce/src/main/java/com/br/eCormmerce/exceptions/GlobalExceptionHandler.java
+++ eCormmerce/src/main/java/com/br/eCormmerce/exceptions/GlobalExceptionHandler.java
@@ -4,8 +4,10 @@ package com.br.eCormmerce.exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,16 +16,27 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(
-            MethodArgumentNotValidException ex, WebRequest request) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((org.springframework.validation.FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ResponseEntity<>("O CPF Informado já esta em uso", HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+        return new ResponseEntity<>("Esse método não é suportado", HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Object> handleValidationExceptions(
+                MethodArgumentNotValidException ex, WebRequest request) {
+            Map<String, String> errors = new HashMap<>();
+            ex.getBindingResult().getAllErrors().forEach((error) -> {
+                String fieldName = ((org.springframework.validation.FieldError) error).getField();
+                String errorMessage = error.getDefaultMessage();
+                errors.put(fieldName, errorMessage);
+            });
+            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        }
 }
