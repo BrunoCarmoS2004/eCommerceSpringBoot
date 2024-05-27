8d3ea435dd02828ae8e700ee735f9b561eb0076d
Bruno do Carmo Souza <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-05-27 16:44:31 -0300

    Iniciando Home

--- frontEndEcommerce/src/app/app.routes.ts
+++ frontEndEcommerce/src/app/app.routes.ts
@@ -16,12 +16,13 @@ export const routes: Routes = [
     path:"home",
     component:HomeComponent
   },
-  /*{
+  {
     path:"**",
     redirectTo:"home", pathMatch: "full"
-  }*/
+  }
+  /*
   {
     path:"**",
     redirectTo:"auth/acesso", pathMatch: "full"
-  }
+  }*/
 ];
--- frontEndEcommerce/src/app/pages/home/home.component.ts
+++ frontEndEcommerce/src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
